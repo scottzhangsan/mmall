df54bee00aa2e857aacd03301898d90f278ca1d0
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2019-03-20 22:57:12 +0800

    init project finish

--- src/main/java/com/springboot/mmall/MmallApplication.java
+++ src/main/java/com/springboot/mmall/MmallApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.mmall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.springboot.mmall.dao"}) //需要扫描的mapper地址
 public class MmallApplication {
 	
 	public static void main(String[] args) {
